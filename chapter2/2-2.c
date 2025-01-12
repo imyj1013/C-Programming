3221e97e17d9b2f44bcfe80f52f6bcbec14c2da2
imyj1013 <imyj1013@gmail.com>
2025-01-13 08:25:37 +0900

    refactor: organize resources

---
+++ chapter2/2-2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	double a = 4.200000;   // 변수 a 선언
+	double b = 5.100000;   // 변수 b 선언
+	printf("first number: %f\n", a);   // 부동소수형으로 출력
+	printf("second number: %f\n", b);  // 부동소수형으로 출력
+	double sum = a + b;   // 변수 sum 선언
+	printf("sum: %f", sum);    // 부동소수형으로 출력
+	return 0;
+}
