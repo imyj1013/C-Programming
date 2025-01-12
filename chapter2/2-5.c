a5d119228ffb160fd6af2d625c8f6f8879ce61ee
imyj1013 <imyj1013@gmail.com>
2025-01-13 08:28:35 +0900

    refactor: organize resources

---
+++ chapter2/2-5.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("The double quotation mark is \".\nThe single quotation mark is \'.\nCharacter A is \'A\'.\nTo represent a double quotation mark, use the escape sequence \\\".");   // 이스케이프 시퀀스를 이용하여 출력
+	return 0;
+}
