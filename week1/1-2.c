77ac37264d5077c3863f625066c8a4bafec1495a
imyj1013 <imyj1013@gmail.com>
2025-01-13 07:58:53 +0900

    refactor

---
+++ week1/1-2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{    
+	printf("   A   \n");   // 첫번째줄    A    출력
+	printf("  A A  \n");   // 두번째줄   A A   출력
+	printf(" A   A \n");   // 세번째줄  A   A  출력
+	printf("A A A A\n");   // 네번째줄 A A A A 출력
+	return 0;
+}
