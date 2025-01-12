c721fe08feb2f69869547269630985b264ae9c9f
imyj1013 <imyj1013@gmail.com>
2025-01-13 08:40:55 +0900

    refactor: organize resources

--- chapter3/3-6.c
+++ chapter3/3-6.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-	int a, b, c;   // 변수 선언
-	scanf("%d/%d/%d", &a, &b, &c);   // 년, 월, 일을 입력받아 a, b, c에 저장
-	printf("year %04d month %02d date %02d", a, b, c);   // 문자와 정수가 혼합된 문장을 출력
+	int a, b, c;  // 변수 선언
+	scanf("%d/%d/%d", &a, &b, &c);  // 년, 월, 일을 입력받아 a, b, c에 저장
+	printf("year %04d month %02d date %02d", a, b, c);  // 문자와 정수가 혼합된 문장을 출력
 	return 0;
 }
