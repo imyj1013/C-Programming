abbc6c4dde988565f8ff06ffdab86bad42150255
imyj1013 <imyj1013@gmail.com>
2025-01-13 08:27:43 +0900

    refactor: organize resources

---
+++ chapter2/2-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;   // 변수 a 선언
+	scanf("%d", &a);    // 값을 입력받아 a에 저장
+	unsigned char ch = a;   // int형 변수 a를 unsigned char형 변수에 복사
+	printf("(int->int) %d\n", a);   // a를 정수형태로 출력
+	printf("(int->unsigned char->int) %d", ch);  // ch를 정수형태로 출력
+	return 0;
+}
