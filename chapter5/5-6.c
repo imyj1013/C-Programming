3182e0d9f90377d261f29f5aec1172d37ff77a23
imyj1013 <imyj1013@gmail.com>
2025-01-14 01:19:30 +0900

    Update 5-6.c

--- chapter5/5-6.c
+++ chapter5/5-6.c
@@ -2,19 +2,26 @@
 
 int main()
 {
-	int a;    // 변수선언
-	scanf("%d", &a);     // 값을 입력받아 저장
-	if (a % 2 == 0 && a % 3 == 0 && a % 5 == 0)     // a가 2, 3, 5로 나누어 떨어진다면
-		printf("A");     // A 출력
-	else if (a % 2 == 0 && a % 3 == 0 && a % 5 != 0)     // a가 2, 3으로만 나누어 떨어진다면
-		printf("B");     // B 출력
-	else if (a % 2 == 0 && a % 3 != 0 && a % 5 == 0)     // a가 2, 5으로만 나누어 떨어진다면
-		printf("C");     // C 출력
-	else if (a % 2 != 0 && a % 3 == 0 && a % 5 == 0)     // a가 3, 5으로만 나누어 떨어진다면
-		printf("D");     // D 출력
-	else if (a % 2 != 0 && a % 3 != 0 && a % 5 != 0)     // a가 2, 3, 5 중 그 어떠한 수로도 나누어지지 않는다면
-		printf("N");     // N 출력
-	else     // a가 2, 3, 5 중 하나로만 나누어 떨어진다면
-		printf("E");     // E 출력
+	int a, b, c;    // 변수선언
+	scanf("%d %d %d", &a, &b, &c);    // 값을 입력받아 저장
+	if (a >= b && a >= c) {    // a가 b 이상이면서 c 이상이라면
+		if (b >= c)    // b가 c 이상이라면
+			printf("%d %d", a, c);    // 최댓값 a, 최솟값 c 출력
+		else    // b가 c보다 작다면
+			printf("%d %d", a, b);    // 최댓값 a, 최솟값 b 출력
+	}
+	else    // a가 최댓값이 아니라면
+		if (b >= c) {    // b가 c 이상이라면
+			if (a >= c)     // a가 c 이상이라면
+				printf("%d %d", b, c);    // 최댓값 b, 최솟값 c 출력
+			else    // a가 c 보다 작다면
+				printf("%d %d", b, a);    // 최댓값 b, 최솟값 a 출력
+		}
+		else     // b가 c 보다 작다면
+			if (a >= b)     // a가 b 이상이라면
+				printf("%d %d", c, b);    // 최댓값 c, 최솟값 b 출력
+			else     // a가 b보다 작다면
+				printf("%d %d", c, a);    // 최댓값 c, 최솟값 a 출력
 	return 0;
+		
 }
