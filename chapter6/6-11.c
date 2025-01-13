63054e403e90bf8e94105417cafb4894e01b09b1
imyj1013 <imyj1013@gmail.com>
2025-01-14 05:18:13 +0900

    refactor: organize resources

---
+++ chapter6/6-11.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main()
+{
+	int N, i, j, a;     // 변수 선언
+	scanf("%d", &N);
+	a = 1;     // 출력할 숫자 a 초기화
+	for (i = 1; i <= N; i++) {     // N개의 행
+		for (j = 1; j <= N; j++) {     // N개의 열
+			printf(" %d", a);     // 공백과 a를 출력
+			if (a == 9)
+				a = 0;     // a가 9라면 다음에 출력할 수는 0
+			else
+				a += 1;     // a가 9가 아니라면 다음에 출력할 수는 a에 1을 더한 수
+		}
+		printf("\n");     // 줄바꿈
+	}
+	return 0;
+}
