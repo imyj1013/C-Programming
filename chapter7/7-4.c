de06fc99cb45d969550672596a5938d1558abd94
imyj1013 <imyj1013@gmail.com>
2025-01-14 05:54:00 +0900

    refactor: organize resources

---
+++ chapter7/7-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main()
+{
+	int i, X[10], Y[10], Z[10], sum;     // 변수 선언
+	for (i = 0; i < 10; i++) {
+		scanf("%d", &X[i]);     // 배열에 정수 입력받음
+	}
+	for (i = 0; i < 10; i++) {
+		scanf("%d", &Y[i]);     // 배열에 정수 입력받음
+	}
+	for (i = 0; i < 10; i++) {
+		Z[i] = X[i] + Y[9-i];     // 배열에 두 정수를 더한 값을 저장
+		printf(" %d", Z[i]);     // 두 정수를 더한 값을 입력한 배열을 출력
+	}
+	return 0;
+}
