37c9b4b2d02a74a6c22a7486e9e91f5319db845e
imyj1013 <imyj1013@gmail.com>
2025-01-14 05:15:33 +0900

    refactor: organize resources

--- chapter6/6-10.c
+++ chapter6/6-10.c
@@ -2,35 +2,16 @@
 
 int main()
 {
-	int N, i, j;
-	scanf("%d", &N);
-	for (i = 1; i <= (N+1)/2 ; i++) {
-		for (j = 0; j <= (i-1) ; j++) {
-			printf("O");
+	int N, i, j;     // 변수선언
+	scanf("%d", &N);     // 값을 입력받아 N에 저장
+	for (i = 1; i <= N; i++) {     // i가 1부터 N인 동안 반복
+		for (j = 1; j <= N; j++) {      // j가 1부터 N인 동안 반복
+			if (i == j || i == N+1-j)     // i가 j와 같거나 (N+1-j)와 같을 때
+				printf("X");     // X를 출력
+			else
+				printf("O");     // O를 출력
 		}
-		printf("X");
-		for (j = 0; j <= (N - 2 * i); j++) {
-			printf("O");
-		}
-		printf("X");
-		for (j = 0; j <= (i - 1); j++) {
-			printf("O");
-		}
-		printf("\n");
-	}
-	for (i = 1; i <= (N + 1) / 2; i++) {
-		for (j = (N-i) ; j >= 1 ; j--) {
-			printf("O");
-		}
-		printf("X");
-		for (j = (2*i-N)-2; j >= 1 ; j--) {
-			printf("O");
-		}
-		printf("X");
-		for (j = (N-i) ; j >= 1 ; j--) {
-			printf("O");
-		}
-		printf("\n");
+		printf("\n");     // 줄바꿈
 	}
 	return 0;
 }
